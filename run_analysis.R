c90a8b4d4399936fabd7053b5b0fc2d7d516bb68
Remco Niggebrugge <remco.niggebrugge@gmail.com>
2015-01-15 00:13:26 +0100

    Working version of run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,104 @@
 ## R script file for the Getting and Cleaning Data Course Project
+
+library(dplyr)          # for grouping and summarizing data.frames
+library(lattice)        # for plotting, not needed but good to have
+                        # at hand while testing
+
+# Set working directory, it should point to folder with the raw data
+# files. Change this if the script is located in different folder.
+
+setwd("/home/remco/Desktop/data/Getting-and-Cleaning-Data-Course-Project/")
+
+# Reading the tables to be merged.
+# - subject      : list with numbers of subjects for each observation
+# - activity     : list of activities performed for each observation
+# - measurements : the actual measurements for each observation
+# - features     : list with titles of all measurements, used to create
+#                  meaningful column names
+
+subject      <- read.table("./train/subject_train.txt")
+activity     <- read.table("./train/y_train.txt")
+measurements <- read.table("./train/X_train.txt", sep="")
+features     <- read.table("./features.txt")
+
+# The "features" table needs a bit of processing. The first colum
+# contains a numeric reference of the measurement, this can be
+# ignored. The second column, with the names of the measurements,
+# contains many unwanted characters, these will be removed before
+# using the values as column names of the dataset.
+
+features <- features[,2]   
+features <- gsub("[)(,\\-]","", features)
+
+# Adding column names to the above data.frames. This will ensure the
+# final table is understandable and ensures a smooth merge with the
+# test-set data 
+
+names(subject)      <- "subject"
+names(activity)     <- "activity"
+names(measurements) <- features    
+
+# Now merge the three dataframes by binding the columns ("left-to-right")
+
+train_frame <- cbind(subject, activity, measurements)
+
+# Repeat the same steps for the tables in ./test/ directory
+
+subject             <- read.table("./test/subject_test.txt")
+activity            <- read.table("./test/y_test.txt")
+measurements        <- read.table("./test/X_test.txt", sep="")
+names(subject)      <- "subject"
+names(activity)     <- "activity"
+names(measurements) <- features
+test_frame          <- cbind(subject, activity, measurements)
+
+# Now merge to train and test data.frames by binding the rows 
+# ("top-to-bottom") the resulting data.frame with all observations over 
+# all subjects is stored in "data"
+
+data <- rbind(train_frame, test_frame)
+
+# The activity column in the data.frame contains numeric values
+# corresponding to the six observed activities. This needs to be
+# replaced with more meaningful labels. These labels can be found
+# in the "activity_labels.txt" file where they are stored in 2nd column.
+
+act_labels     <- read.table("./activity_labels.txt")
+act_labels$V2  <- tolower(labels$V2)
+data$activity  <- factor(data$activity, labels=act_labels[,2])
+
+# Using again the "features" list with all measurements, select those
+# representing 'mean' and 'standard deviations' values. 
+# The 'grep' function will return a list with column numbers we need
+# to keep in our data.frame. In order to take the first two columns
+# into account (subject and activity) the discovered column numbers
+# are all increased with 2, and the numbers 1 and 2 are added to list
+# of columns we need to keep.
+
+columns <- grep("[[Mm]ean|std]", features) 
+columns <- columns +2       
+columns <- c(1,2,columns)   
+
+# With that we can reduce are "data" data.frame, only keeping 
+# the columns as described above.
+
+data <- data[,columns]
+
+# As a final step we will further reduce the data.frame by grouping
+# over subjects and activities. For each subject-activity combination
+# the mean value for each measurements is calculated. This will result
+# in 180 observations (6 activities * 30 subjects).
+#
+# The function "summarise_each" is used to summarize over all columns
+# other than those grouped by.
+
+new_data_set <- group_by(data, subject, activity) %>% summarise_each(funs(mean))
+
+# Let's inspect the result a bit:
+
+with(new_data_set, xyplot(tBodyAccmeanX ~  activity | subject ))
+with(new_data_set, xyplot(tBodyAccmeanX ~  subject | activity ))
+
+str(new_data_set)
+summary(new_data_set)
+new_data_set
